49ab7a982d3ab860222e7feda62204e19acbbb19
yousri <eng.yousrii@gmail.com>
2021-04-20 07:05:00 +0200

    Student struct added

---
+++ Structures.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURES_H_INCLUDED
+#define STRUCTURES_H_INCLUDED
+#define NULL 0
+struct student{
+    char name[40];
+    int id;
+    int d , m , y;
+    int last_score;
+};
+
+
+
+#endif // STRUCTURES_H_INCLUDED
